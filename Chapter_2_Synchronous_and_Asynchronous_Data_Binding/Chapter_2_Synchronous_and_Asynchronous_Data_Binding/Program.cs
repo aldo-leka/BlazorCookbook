594208948bed82663f95c7c11e1ac6c3b93e2dfb
Aldo Leka <aldo.leka@live.com>
2025-05-23 14:40:51 +0200

    init ch.2

---
+++ Chapter_2_Synchronous_and_Asynchronous_Data_Binding/Chapter_2_Synchronous_and_Asynchronous_Data_Binding/Program.cs
@@ -0,0 +1,36 @@
+using Chapter_2_Synchronous_and_Asynchronous_Data_Binding.Client.Pages;
+using Chapter_2_Synchronous_and_Asynchronous_Data_Binding.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Chapter_2_Synchronous_and_Asynchronous_Data_Binding.Client._Imports).Assembly);
+
+app.Run();

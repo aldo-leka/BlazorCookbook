551acde2df2ad02d3c89fa3d7551fa390dfa60d9
Aldo Leka <aldo.leka@live.com>
2025-05-27 08:30:20 +0200

    Ch. 7 recipe 05 - styling validation messages

---
+++ Chapter_7_Validating_User_Input_Forms/Chapter_7_Validating_User_Input_Forms.Client/Recipe05/Event.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Chapter_7_Validating_User_Input_Forms.Client.Recipe05;
+
+public class Event
+{
+    [EventNameValidation]
+    public string Name { get; set; }
+    
+    [ValidateComplexType]
+    public EventLocation Location { get; set; } = new();
+}

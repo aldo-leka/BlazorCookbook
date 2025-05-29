5a551f431821012329f9b984a34e260b7b568df0
Aldo Leka <aldo.leka@live.com>
2025-05-29 12:18:16 +0200

    Ch. 9 recipe 04 - constant code-behind values for routing

---
+++ Chapter_9_Exploring_Navigation_and_Routing/Chapter_9_Exploring_Navigation_and_Routing.Client/Recipe04/DeepLinks.cs
@@ -0,0 +1,10 @@
+namespace Chapter_9_Exploring_Navigation_and_Routing.Client.Recipe04;
+
+public static class DeepLinks
+{
+    public const string
+        LandingPage = "/ch09r04",
+        EventPage = "/ch09r04/{eventId:guid}",
+        EventAtVenuePage =
+            "/ch09r04/{eventId:guid}/venues/{venue?}";
+}

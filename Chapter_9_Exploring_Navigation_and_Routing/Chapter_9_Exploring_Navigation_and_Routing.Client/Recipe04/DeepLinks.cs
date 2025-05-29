fe7ccda15f95879e23f80a7b9f7a3979d1265774
Aldo Leka <aldo.leka@live.com>
2025-05-29 12:20:36 +0200

    Ch. 9 recipe 04 - using Deep links static class also for page url generation

--- Chapter_9_Exploring_Navigation_and_Routing/Chapter_9_Exploring_Navigation_and_Routing.Client/Recipe04/DeepLinks.cs
+++ Chapter_9_Exploring_Navigation_and_Routing/Chapter_9_Exploring_Navigation_and_Routing.Client/Recipe04/DeepLinks.cs
@@ -7,4 +7,7 @@ public static class DeepLinks
         EventPage = "/ch09r04/{eventId:guid}",
         EventAtVenuePage =
             "/ch09r04/{eventId:guid}/venues/{venue?}";
+    
+    public static string GetPage(Guid eventId)
+        => EventPage.Replace("{eventId:guid}", $"{eventId}");
 }
